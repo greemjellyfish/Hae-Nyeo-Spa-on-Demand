e6c99dfbcc2a9b56053a5022a3099b669436fd73
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-22 20:52:50 -0400

    HomePage bug fixed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import { Card, Button, Icon } from 'semantic-ui-react'
+import { BrowserRouter as  Link} from "react-router-dom";
 
 const HomePage = () => {
 
@@ -27,7 +26,7 @@ const HomePage = () => {
   
   <div class="blob">
    <svg class="blob3" xmlns="http://www.w3.org/2000/svg" width="600" height="600" viewBox="0 0 600 600"><path d="M477.2 201.2C504.4 244.7 483.6 319.3 446.1 379 408.5 438.7 354.3 483.3 293.8 486.9 233.3 490.5 166.6 453 139.9 399.6 113.2 346.2 126.5 276.8 159.8 229.8 193.2 182.7 246.6 157.8 310.8 151.6 375.1 145.3 450.1 157.7 477.2 201.2Z" fill="#FE840E"/></svg>
-    <h1></h1>
+    
   </div>
   
 
