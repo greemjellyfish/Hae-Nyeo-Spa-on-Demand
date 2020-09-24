d4666643500299fff57d20dddd69321b72b6ddc6
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-24 00:02:13 -0400

    fetch update w heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 export const getTreatments = () => {
-  return fetch(`http://localhost:3000/api/v1/treatments`)
+  return fetch(`https://haenyeospa.herokuapp.com/api/v1/treatments`)
     .then(r => r.json())
 }
 
 export const getAppointment = () => {
-  return fetch(`http://localhost:3000/api/v1/users/1/appointments`)
+  return fetch(`https://haenyeospa.herokuapp.com/api/v1/users/1/appointments`)
     .then(r => r.json())
 }
 
 export const addAppointment = () => {
-  return fetch(`http://localhost:3000/api/v1/users/1/appointments`, {
+  return fetch(`https://haenyeospa.herokuapp.com/api/v1/users/1/appointments`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
