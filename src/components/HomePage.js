cd0e1dff042ab6a877fc002a532405a9d925a701
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-25 12:01:00 -0400

    font test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -16,7 +16,7 @@ const HomePage = () => {
 
   <div class="blob">
     <svg class="blob1" xmlns="http://www.w3.org/2000/svg" width="600" height="600" viewBox="0 0 600 600"><path d="M480.8 168.7C524.5 210.6 543.4 285.2 528.9 353.7 514.5 422.2 466.7 484.5 409.7 502.2 352.6 519.8 286.4 492.8 237.5 458.3 188.6 423.8 157 381.8 142.2 331.7 127.4 281.7 129.2 223.5 158.3 184.9 187.4 146.2 243.7 127.1 306.1 122.2 368.5 117.3 437.1 126.7 480.8 168.7Z" fill="#FE840E"/>   </svg>
-   <h1><Link to="/treatments">TREATMENT</Link></h1>
+   <h1><Link to="/treatments">T w REATMENT</Link></h1>
   </div>
   
   <div class="blob">      
