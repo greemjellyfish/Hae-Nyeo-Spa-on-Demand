355d452733725b0691a6ac097b76fd233f65a9cd
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-24 22:21:14 -0400

    REV AppointmentCard

--- src/components/appointment/AppointmentCard.js
+++ src/components/appointment/AppointmentCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as  Link, withRouter } from "react-router-dom"
+import { BrowserRouter as  Router, Switch, Route,  Link, withRouter } from "react-router-dom"
 
 
 const AppointmentCard = ({appointment, deleteAppointment}) => {
